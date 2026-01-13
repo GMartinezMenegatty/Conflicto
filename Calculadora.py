34b953f71dc7e1490047c1b54ff2dd6647aad6f7
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-01-13 13:42:21 +0100

    Cambio realizado

--- Calculadora.py
+++ Calculadora.py
@@ -2,7 +2,7 @@ class CalculadoraBinaria:
     def __init__(self, a, b):
         self.set_a(a)
         self.set_b(b)
-#Comentario 3
+#Comentario 4
     def set_a(self, a):
         if isinstance (a, int) or isinstance(a, float) or isinstance(a, complex):
             self.__a = a
